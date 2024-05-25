a41f2324ea0b2323b4378e14bc0ffc3ee808fdee
bragaLeandro <leandro384650@gmail.com>
2024-05-25 15:17:40 -0300

    feat: transaction controller

--- src/main/java/br/com/ibm/controller/TransactionController.java
+++ src/main/java/br/com/ibm/controller/TransactionController.java
@@ -1,2 +1,41 @@
-package br.com.ibm.controller;public class TransactionController {
+package br.com.ibm.controller;
+
+
+import br.com.ibm.dto.PixTransactionDto;
+import br.com.ibm.dto.TransactionPixTransactionDto;
+import br.com.ibm.exception.InactiveUserException;
+import br.com.ibm.exception.InsufficientBalanceException;
+import br.com.ibm.service.PixTransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final PixTransactionService pixTransactionService;
+
+    public TransactionController(PixTransactionService pixTransactionService) {
+        this.pixTransactionService = pixTransactionService;
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<String> makeTransfer(@RequestBody PixTransactionDto pixTransactionDto) {
+        try {
+            this.pixTransactionService.makeTransference(pixTransactionDto);
+            return ResponseEntity.ok("Transferência finalizada");
+        } catch (InactiveUserException | InsufficientBalanceException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError().body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<TransactionPixTransactionDto>> getHistoryTransactions(@RequestParam("id") Long userId) {
+        List<TransactionPixTransactionDto> transactions = this.pixTransactionService.getHistoryTransactions(userId);
+        return ResponseEntity.ok(transactions);
+    }
 }
