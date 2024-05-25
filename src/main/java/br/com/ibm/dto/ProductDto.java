74c380478798eb4e371b0ef994c5106fdae49db3
bragaLeandro <leandro384650@gmail.com>
2024-05-25 15:49:21 -0300

    feat: productDto

--- src/main/java/br/com/ibm/dto/ProductDto.java
+++ src/main/java/br/com/ibm/dto/ProductDto.java
@@ -1,2 +1,25 @@
-package br.com.ibm.dto;public class ProductDto {
+package br.com.ibm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+
+    private UUID uuid;
+    private String name;
+    private LocalDate creationDate;
+    private LocalDate updateDate;
+    private boolean enable;
+
+    public ProductDto(String name, boolean enable) {
+        this.name = name;
+        this.enable = enable;
+    }
 }
