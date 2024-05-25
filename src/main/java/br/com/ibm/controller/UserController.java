15defb6149abc40ef9b41fbf8747b25b896e2505
bragaLeandro <leandro384650@gmail.com>
2024-05-25 13:56:44 -0300

    feat: new userController

--- src/main/java/br/com/ibm/controller/UserController.java
+++ src/main/java/br/com/ibm/controller/UserController.java
@@ -1,2 +1,57 @@
-package br.com.ibm.controller;public class UserController {
+package br.com.ibm.controller;
+
+import br.com.ibm.dto.UserDto;
+import br.com.ibm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
+        try {
+            this.userService.createUser(userDto);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException ie) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ie.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
+    }
+
+    @PutMapping("/inactivate")
+    public ResponseEntity<String> inactivateUser(@RequestParam("id") Long id) {
+        try {
+            this.userService.inactivateUser(id);
+            return ResponseEntity.ok("User deactivated");
+        } catch (IllegalArgumentException ie) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ie.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
+    }
+
+    @PutMapping("/activate")
+    public ResponseEntity<String> activateUser(@RequestParam("id") Long id) {
+        try {
+            this.userService.activateUser(id);
+            return ResponseEntity.ok("User activated");
+        } catch (IllegalArgumentException ie) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ie.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
+    }
 }
