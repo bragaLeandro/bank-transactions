0f87d09435c291ec073bd96597be780d7a14daaa
bragaLeandro <leandro384650@gmail.com>
2024-05-25 15:16:16 -0300

    feat: jpa queries in repositories

--- src/main/java/br/com/ibm/repository/PixKeyRepository.java
+++ src/main/java/br/com/ibm/repository/PixKeyRepository.java
@@ -3,7 +3,10 @@ package br.com.ibm.repository;
 import br.com.ibm.entity.PixKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PixKeyRepository extends JpaRepository<PixKey, UUID> {
+
+    Optional<PixKey> findPixKeyByKeyValue(String keyValue);
 }
--- src/main/java/br/com/ibm/repository/ProductRepository.java
+++ src/main/java/br/com/ibm/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package br.com.ibm.repository;
 import br.com.ibm.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    Optional<Product> findProductByName(String name);
 }
