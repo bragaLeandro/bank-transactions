d855425283fd9e2ab67243b916d10b2c6583338e
bragaLeandro <leandro384650@gmail.com>
2024-05-25 13:56:50 -0300

    feat: new services

--- src/main/java/br/com/ibm/service/BalanceService.java
+++ src/main/java/br/com/ibm/service/BalanceService.java
@@ -1,2 +1,18 @@
-package br.com.ibm.service;public class BalanceService {
+package br.com.ibm.service;
+
+import br.com.ibm.entity.Balance;
+import br.com.ibm.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class BalanceService {
+
+    public Balance setInicialBalance(User user) {
+        Balance initialBalance = new Balance();
+        initialBalance.setUser(user);
+        initialBalance.setValue(BigDecimal.ZERO);
+        return initialBalance;
+    }
 }
--- src/main/java/br/com/ibm/service/UserService.java
+++ src/main/java/br/com/ibm/service/UserService.java
@@ -1,2 +1,81 @@
-package br.com.ibm.service;public class UserService {
+package br.com.ibm.service;
+
+import br.com.ibm.dto.UserDto;
+import br.com.ibm.entity.Balance;
+import br.com.ibm.entity.Product;
+import br.com.ibm.entity.User;
+import br.com.ibm.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BalanceService balanceService;
+
+    private final ModelMapper modelMapper;
+
+    public UserService(UserRepository userRepository, BalanceService balanceService, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.balanceService = balanceService;
+        this.modelMapper = modelMapper;
+    }
+
+    @Transactional
+    public void createUser(UserDto userDto) {
+
+        User user = modelMapper.map(userDto, User.class);
+
+        Balance initialBalance = balanceService.setInicialBalance(user);
+        user.setBalance(initialBalance);
+
+        userRepository.save(user);
+    }
+
+    public void inactivateUser(Long id) {
+        User user = this.findById(id);
+
+        if (!user.isEnabled()) {
+            throw new IllegalArgumentException("User is already disabled");
+        }
+
+        user.setEnable(false);
+
+        userRepository.save(user);
+    }
+
+    public void activateUser(Long id) {
+        User user = this.findById(id);
+
+        if (user.isEnabled()) {
+            throw new IllegalArgumentException("User is already enabled");
+        }
+
+        user.setEnable(true);
+
+        userRepository.save(user);
+    }
+
+    public void removeProductFromUser(Long userId, UUID productId) {
+        User user = this.findById(userId);
+
+        Product product = user.getProducts().stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Product not found in user's products list"));
+
+        user.getProducts().remove(product);
+
+        userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
 }
